cc0a813f8d50922d8704ab873f6abf665799085f
Jesse Thomas <jessemacfarlane@gmail.com>
2018-12-14 06:43:29 -0600

    Update - interfacing with HTML and Flask

--- database.py
+++ database.py
@@ -4,6 +4,22 @@ conn = sqlite3.connect('data.db')
 
 c = conn.cursor()
 
+#Create the subcatagory database
+# c.execute('''CREATE TABLE subcatagory
+#              (name text, subtable text)''')
+#
+# # Insert orings into database
+# c.execute("INSERT INTO subcatagory VALUES ('orings', 'as568')")
+# c.execute("INSERT INTO subcatagory VALUES ('mfs', 'dwgnum')")
+# c.execute("INSERT INTO subcatagory VALUES ('oilseals', 'profiles')")
+
+
+#Fetch and print -001 data
+# c.execute("SELECT * FROM subcatagory")
+# #
+# print(c.fetchall())
+
+
 #Create the as568 database
 # c.execute('''CREATE TABLE as568
 #              (dash text, id float, od float, cs float)''')
--- routes.py
+++ routes.py
@@ -1,10 +1,25 @@
 from flask import Flask, render_template
 from flask_bootstrap import Bootstrap
+from flask_sqlalchemy import SQLAlchemy
+from flask_wtf import FlaskForm
+from wtforms import SelectField
 
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///test.db'
+app.config['SECRET_KEY'] = 'secret'
+
+db = SQLAlchemy(app)
 
 Bootstrap(app)
 
-@app.route('/')
+class Form(FlaskForm):
+    product_catagory = SelectField('product_catagory', choices=[('OR','O-rings'), ('MFS', 'Metal Face Seals'), ('OS', 'Oil Seals')])
+    subcatagory = SelectField('subcatagory', choices=[])
+@app.route('/', methods=['GET', 'POST'])
 def home():
-        return render_template('home.html')
+    form = Form()
+    form.subcatagory.choices = [(subcatagory.id, subcatagory.name) for subcatagory in subcatagory.query.filter_by(name = 'orings').all()] #NEED TO USE THE MASTER DB NOT AS568
+    return render_template('home.html')
+
+if __name__ == '__main__':
+    app.run(debug=True)
